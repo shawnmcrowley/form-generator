2e3233c720d3aa2103dd9baeeed2193f64f8e2c6
shawnmcrowley <scrowley@buffalo.edu>
2025-12-30 18:53:57 -0500

    Clean up nested form-generator directory structure

--- form-generator/src/app/components/header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-    return (
-        <div className="h-full max-h-64 flex items-center justify-center">
-            <h1>Form Generator</h1>
-        </div>
-    )
-}
